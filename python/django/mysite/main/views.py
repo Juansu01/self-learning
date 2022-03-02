3ef1ba613f14741e11011f0148dc1eaf5f6332da
Juansu01 <thejuank0@hotmail.com>
2022-03-02 15:06:19 -0500

    Adding changes to views

--- python/django/mysite/main/views.py
+++ python/django/mysite/main/views.py
@@ -6,9 +6,9 @@ from .models import ToDoList, Item
 
 def index(response, id):
     ls =  ToDoList.objects.get(id=id)
-    return render(response, "main/base.html", {"name":ls.name})
+    return render(response, "main/list.html", {"ls": ls})
 
 
 def home(response):
-    return render(response, "main/home.html", {"name":"test"})
+    return render(response, "main/home.html", {})
 
