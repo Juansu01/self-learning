9c893b9f7cdb85cd6f13c94f44643de4ef21a796
Juansu01 <thejuank0@hotmail.com>
2022-03-12 09:58:35 -0500

    Adding new views for submitting forms

--- python/django/mysite/main/views.py
+++ python/django/mysite/main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import ToDoList, Item
+from .forms import CreateList
 # Create your views here.
 
 
@@ -12,3 +13,19 @@ def index(response, id):
 def home(response):
     return render(response, "main/home.html", {})
 
+
+def create(response):
+    if response.method == "POST":
+        form = CreateList(response.POST)
+
+        if form.is_valid():
+            n = form.cleaned_data["name"]
+            t = ToDoList(name=n)
+            t.save()
+            print("Created new ToDoList.")
+
+        return HttpResponseRedirect("/%i" %t.id)
+
+    else:
+        form = CreateList()
+    return render(response, "main/create.html", {"form": form})
